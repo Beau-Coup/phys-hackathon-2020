e62268c37137f3d45a36cd57442d911cdbc24086
PeterKaloyannis <peter.kaloyannis@outlook.com>
2020-11-08 10:30:48 -0500

    yumyyumy

--- basic_ray_tracer.py
+++ basic_ray_tracer.py
@@ -25,11 +25,11 @@ for i in range(1,6):
     points = np.load(input_path+str(i)+".npz","r+")["arr_0"]
     Fullpoints = np.zeros([points.shape[1],3]) 
 
-    Fullpoints[:,0] = points[0,:]
+    Fullpoints[:,0] = points[0,:]+10*i
     Fullpoints[:,1] = points[1,:]
 
     
-    plot.set_data("flake"+str(i),Fullpoints+i*6,rad, c=[0.7,0.7,1],mat="ice",rnd=False, geom=Geometry(8),)
+    plot.set_data("flake"+str(i),Fullpoints,rad, c=[0.7,0.7,1],mat="ice",rnd=False, geom=Geometry(8))
 # n = 100000                             # 1M points, better not try this with matplotlib
 # xyz = 3 * (np.random.random((n, 3)) - 0.5)   # random 3D positions
 # r = 0.02 * np.random.random(n) + 0.002       # random radii
