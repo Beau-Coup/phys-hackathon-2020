c2cbc63b1b8f9add1c0639375f6c4a4e197a1cac
PeterKaloyannis <peter.kaloyannis@outlook.com>
2020-11-08 11:56:58 -0500

    sss

--- basic_ray_tracer.py
+++ basic_ray_tracer.py
@@ -41,18 +41,18 @@ plot.set_background_mode("TextureEnvironment")
 plot.set_background(fname)
 plot.set_ambient(0.1)
 
-for i in range(1,6):
+# for i in range(1,6):
     # points = np.load(input_path+str(i)+".npz","r+")["arr_0"]
-    points = generate_snowflake().T
+points = generate_snowflake().T
 
-    Fullpoints = np.zeros([points.shape[1],3]) 
+Fullpoints = np.zeros([points.shape[1],3]) 
 
-    Fullpoints[:,0] = points[0,:]+10*i
-    Fullpoints[:,1] = points[1,:]
+Fullpoints[:,0] = points[0,:]#+10*i
+Fullpoints[:,1] = points[1,:]
     # Fullpoints[:,2] = 2*i
 
     
-    plot.set_data("flake"+str(i),Fullpoints,rad, c=[0.85,0.85,1],mat="ice",rnd=False, geom=Geometry(8))
+plot.set_data("flake"+str(1),Fullpoints,rad, c=[0.85,0.85,1],mat="ice",rnd=False, geom=Geometry(8))
     
 # n = 100000                             # 1M points, better not try this with matplotlib
 # xyz = 3 * (np.random.random((n, 3)) - 0.5)   # random 3D positions
