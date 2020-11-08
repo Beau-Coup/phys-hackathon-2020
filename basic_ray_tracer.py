b5b653e13a5ac24e887f0d3e80c0e48dfd32b096
PeterKaloyannis <peter.kaloyannis@outlook.com>
2020-11-08 09:54:07 -0500

    more stuff

--- basic_ray_tracer.py
+++ basic_ray_tracer.py
@@ -1,10 +1,13 @@
 import numpy as np
-from plotoptix import TkOptiX
+from plotoptix import TkOptiX,NpOptiX
 from plotoptix.enums import Geometry
+from plotoptix.materials import m_dispersive_glass
 
 
 
-rad = 0.2
+
+
+rad = 0.13
 
 points = np.load("full_save.npz","r+")["arr_0"]
 Fullpoints = np.zeros([points.shape[1],3])
@@ -13,7 +16,8 @@ Fullpoints[:,0] = points[0,:]
 Fullpoints[:,1] = points[1,:]
 
 plot = TkOptiX()
-plot.set_data("myplot",Fullpoints,rad, c=[0.7,0.7,1])#, geom=Geometry(8))
+plot.setup_material("ice",m_dispersive_glass)
+plot.set_data("myplot",Fullpoints,rad, c=[0.7,0.7,1],mat="ice",rnd=False)#, geom=Geometry(8),)
 # n = 100000                             # 1M points, better not try this with matplotlib
 # xyz = 3 * (np.random.random((n, 3)) - 0.5)   # random 3D positions
 # r = 0.02 * np.random.random(n) + 0.002       # random radii
